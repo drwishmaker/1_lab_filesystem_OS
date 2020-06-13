26c1222e74a46f748b74cba516fc056783de5780
Artemy Krupsky <4xel1337@gmail.com>
2020-06-13 22:22:46 +0300

    v1.0

---
+++ Laba_OS_Filesystem/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Laba_OS_Filesystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDefault = new System.Windows.Forms.TextBox();
+            this.textBoxProceeded = new System.Windows.Forms.TextBox();
+            this.buttonFile = new System.Windows.Forms.Button();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxDefault
+            // 
+            this.textBoxDefault.Location = new System.Drawing.Point(12, 23);
+            this.textBoxDefault.Multiline = true;
+            this.textBoxDefault.Name = "textBoxDefault";
+            this.textBoxDefault.ReadOnly = true;
+            this.textBoxDefault.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxDefault.Size = new System.Drawing.Size(275, 260);
+            this.textBoxDefault.TabIndex = 0;
+            // 
+            // textBoxProceeded
+            // 
+            this.textBoxProceeded.Location = new System.Drawing.Point(293, 23);
+            this.textBoxProceeded.Multiline = true;
+            this.textBoxProceeded.Name = "textBoxProceeded";
+            this.textBoxProceeded.ReadOnly = true;
+            this.textBoxProceeded.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxProceeded.Size = new System.Drawing.Size(278, 260);
+            this.textBoxProceeded.TabIndex = 1;
+            // 
+            // buttonFile
+            // 
+            this.buttonFile.Location = new System.Drawing.Point(12, 289);
+            this.buttonFile.Name = "buttonFile";
+            this.buttonFile.Size = new System.Drawing.Size(559, 23);
+            this.buttonFile.TabIndex = 2;
+            this.buttonFile.Text = "Choose file";
+            this.buttonFile.UseVisualStyleBackColor = true;
+            this.buttonFile.Click += new System.EventHandler(this.buttonFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input data";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(293, 4);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(37, 13);
+            this.result.TabIndex = 4;
+            this.result.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(583, 322);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonFile);
+            this.Controls.Add(this.textBoxProceeded);
+            this.Controls.Add(this.textBoxDefault);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.TextBox textboxProceeded;
+        private System.Windows.Forms.TextBox textboxDefault;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.TextBox textBoxDefault;
+        private System.Windows.Forms.TextBox textBoxProceeded;
+        private System.Windows.Forms.Button buttonFile;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ Laba_OS_Filesystem/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba_OS_Filesystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFile_Click(object sender, EventArgs e)
+        {
+            string text;
+            int count = 0;
+            string[] arrayLine = null;
+            string outputText = "";
+            string firstLine;
+            string[] arrayCluster = null;
+            string[] alphabet = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Z", "V", "Y" };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = ofd.FileName;
+
+                try
+                {
+                    using (var Stream = new StreamReader(fileName, Encoding.UTF8))
+                    {
+                        text = Stream.ReadToEnd();
+                        textBoxDefault.Text = "Your Filesystem:" + Environment.NewLine + text + Environment.NewLine + "Your Files:" + Environment.NewLine;
+
+                        firstLine = File.ReadLines(fileName).Skip(0).First();
+                        string[] elemsOfFirstLine = firstLine.Split(' ');
+
+                        string secondLine = File.ReadLines(fileName).Skip(1).First();
+                        arrayCluster = new string[(secondLine.Length + 1) / 2];
+                        arrayCluster = secondLine.Split(' ');
+                        string[] arrayClusterCopy = secondLine.Split(' ');
+
+                        count = elemsOfFirstLine.Length / 2 + 1;
+                        arrayLine = new string[count];
+                        int i = 0;
+
+                        for (int j = 0; j != arrayLine.Length; j++)
+                        {
+                            if (i < elemsOfFirstLine.Length)
+                            {
+                                arrayLine[j] = elemsOfFirstLine[i] + " " + elemsOfFirstLine[i + 1];
+                                int startValue = Convert.ToInt32(elemsOfFirstLine[i + 1]);
+                                string nextValue = arrayCluster[startValue];
+
+                                while (nextValue != "eof" && nextValue != "bad" && nextValue != "0")
+                                {
+                                    arrayLine[j] += " " + nextValue;
+                                    startValue = Convert.ToInt32(arrayCluster[startValue]);
+                                    nextValue = arrayCluster[startValue];
+                                }
+
+                                if (nextValue == "eof")
+                                {
+                                    arrayLine[j] += " eof";
+                                }
+
+                                if (nextValue == "bad")
+                                {
+                                    arrayLine[j] += " bad";
+                                }
+                                i += 2;
+                                textBoxDefault.Text += arrayLine[j] + Environment.NewLine;
+                            }
+                        }
+
+                        for (int j = 0; j != elemsOfFirstLine.Length; j += 2)
+                        {
+                            int firstValue = Convert.ToInt32(elemsOfFirstLine[j + 1]);
+                            string nextValue = arrayClusterCopy[firstValue];
+
+                            while (nextValue != "eof" && nextValue != "bad" && nextValue != "0")
+                            {
+                                int indexOfCopyArray = firstValue;
+                                firstValue = Convert.ToInt32(nextValue);
+                                nextValue = arrayClusterCopy[firstValue];
+                                arrayClusterCopy[indexOfCopyArray] = "0";
+                            }
+
+                            if (nextValue == "eof")
+                            {
+                                arrayClusterCopy[firstValue] = "0";
+                            }
+
+                            if (nextValue == "bad")
+                            {
+                                arrayClusterCopy[firstValue] = "0";
+                            }
+                        }
+
+                        string otherLine = "";
+
+                        for (int j = 0; j != arrayClusterCopy.Length; j++)
+                        {
+                            if (arrayClusterCopy[j] != "0")
+                            {
+                                if (otherLine == "")
+                                {
+                                    otherLine = Convert.ToString(j);
+                                    int firstValue = j;
+                                    string nextValue = arrayClusterCopy[firstValue];
+
+                                    while (nextValue != "eof" && nextValue != "bad" && nextValue != "0")
+                                    {
+                                        int tempValue = firstValue;
+                                        otherLine += " " + nextValue;
+                                        firstValue = Convert.ToInt32(arrayClusterCopy[firstValue]);
+                                        nextValue = arrayClusterCopy[firstValue];
+                                        arrayClusterCopy[tempValue] = "0";
+                                    }
+
+                                    if (nextValue == "eof")
+                                    {
+                                        otherLine += " eof";
+                                        arrayClusterCopy[firstValue] = "0";
+                                    }
+
+                                    if (nextValue == "bad")
+                                    {
+                                        otherLine += " bad";
+                                        arrayClusterCopy[firstValue] = "0";
+                                    }
+                                }
+
+                                else
+                                {
+                                    int indexOfSubstring = otherLine.IndexOf(arrayClusterCopy[j]);
+                                    otherLine = otherLine.Insert(indexOfSubstring, arrayClusterCopy[j] + " ");
+                                }
+                            }
+                        }
+
+                        arrayLine[arrayLine.Length - 1] = otherLine;
+                        textBoxDefault.Text += arrayLine[arrayLine.Length - 1] + Environment.NewLine;
+                    }
+
+                    int[] arrayOfEmptyClusters = new int[32];
+
+                    for (int j = 0; j != arrayOfEmptyClusters.Length; j++)
+                    {
+                        arrayOfEmptyClusters[j] = 0;
+                    }
+
+                    bool firstSign = false;
+                    bool overlay = false;
+
+                    for (int i = 0; i != arrayLine.Length; i++)
+                    {
+                        if (arrayLine[i] != null && arrayLine[i] != "")
+                        {
+                            string line = arrayLine[i];
+                            string[] elemOfFiles = line.Split(' ');
+                            textBoxProceeded.Text += line;
+                            bool Yes = false;
+
+                            for (int j = 0; j != elemOfFiles.Length; j++)
+                            {
+                                if (Yes == false)
+                                {
+                                    if (elemOfFiles[0] == alphabet[i])
+                                    {
+                                        outputText += elemOfFiles[0];
+                                    }
+
+                                    else
+                                    {
+                                        outputText += alphabet[i];
+                                        firstLine += " " + alphabet[i] + " " + elemOfFiles[0];
+                                        textBoxProceeded.Text += Environment.NewLine + "File was lost in your filesystem. It was added under name '" + alphabet[i] + "'" + Environment.NewLine;
+                                        firstSign = true;
+                                    }
+
+                                    Yes = true;
+                                }
+                               
+                                if (elemOfFiles[j] != alphabet[i] && elemOfFiles[j] != "eof" && elemOfFiles[j] != "bad")
+                                {
+                                    int k = Convert.ToInt32(elemOfFiles[j]);
+
+                                    if (arrayOfEmptyClusters[k] == 0)
+                                    {
+                                        arrayOfEmptyClusters[k] = k;
+                                        outputText += " " + elemOfFiles[j];
+                                    }
+
+                                    else
+                                    {
+                                        overlay = true;
+                                        textBoxProceeded.Text += Environment.NewLine + "Crossing of adresses " + elemOfFiles[j] + ". Duplicate elements will be rewritten to empty cells" + Environment.NewLine;
+                                        bool record = false;
+
+                                        for (int a = 0; a != arrayOfEmptyClusters.Length; a++)
+                                        {
+                                            if (record == false)
+                                            {
+                                                if (arrayOfEmptyClusters[a] == 0)
+                                                {
+                                                    record = true;
+                                                    arrayOfEmptyClusters[a] = k;
+                                                    outputText += " " + a;
+                                                    arrayCluster[Convert.ToInt32(elemOfFiles[j - 1])] = Convert.ToString(a);
+                                                    arrayCluster[a] = "eof";
+                                                    elemOfFiles[j] = Convert.ToString(a);
+                                                }
+                                            }
+                                        }
+
+                                        record = false;
+                                    }
+                                }
+
+                                else
+                                {
+                                    if (elemOfFiles[j] == "eof")
+                                    {
+                                        if (firstSign == false && overlay == false)
+                                        {
+                                            textBoxProceeded.Text += Environment.NewLine + "No errors found" + Environment.NewLine;
+                                        }
+                                        outputText += " " + "eof" + Environment.NewLine;
+                                    }
+
+                                    if (elemOfFiles[j] == "bad")
+                                    {
+                                        textBoxProceeded.Text += Environment.NewLine + "Found a badblock. Deleting this file" + Environment.NewLine;
+                                        outputText += " " + "eof" + Environment.NewLine;
+                                        arrayCluster[Convert.ToInt32(elemOfFiles[j - 1])] = "eof";
+                                    }
+                                }
+                                if (j == elemOfFiles.Length - 1)
+                                {
+                                    if (elemOfFiles[j] != "eof" && elemOfFiles[j] != "bad")
+                                    {
+                                        textBoxProceeded.Text += Environment.NewLine + "The end of the file was omitted. This error will be corrected by writing 'Eof' to the last cell." + Environment.NewLine;
+                                        outputText += " " + "eof" + Environment.NewLine;
+                                        arrayCluster[Convert.ToInt32(elemOfFiles[j])] = "eof";
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    textBoxProceeded.Text += Environment.NewLine + "Fixed filesystem:" + Environment.NewLine + outputText + Environment.NewLine;
+
+
+                }
+
+                catch
+                {
+                    MessageBox.Show("Bad data from file");
+                }
+
+            }
+
+            else
+            {
+                MessageBox.Show("Unable to read file");
+            }
+        }
+
+    }
+}
